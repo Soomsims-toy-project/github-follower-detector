fa98f7c4f9042f3c618f48e0afdf6d2c1ab25874
se0jinYoon <tjwls1999@gmail.com>
2024-06-10 19:06:51 +0900

    feat: header 로고 라우팅 연결

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 import GithubIcon from '@/assets/svgs/github.svg';
 
 function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return (
     <>
       <header className="font-Pretendard flex items-center bg-dark_grey p-8 text-center text-[2rem]">
-        <GithubIcon className="mr-10 h-20 w-20 cursor-pointer" />
+        <GithubIcon onClick={() => router.push('/')} className="mr-10 h-20 w-20 cursor-pointer" />
         깃허브 팔로우 탐지기
       </header>
       {children}

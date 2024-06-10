f4b75164029183ec883add8e629af48f991af474
se0jinYoon <tjwls1999@gmail.com>
2024-06-10 18:11:54 +0900

    feat: 레이아웃 헤더 구현

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import GithubIcon from '@/assets/svgs/github.svg';
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="bg-dark_grey p-8 text-center text-[2rem]">헤더</header>
+      <header className="flex items-center bg-dark_grey p-8 text-center text-[2rem]">
+        <GithubIcon className="h-20 w-20 cursor-pointer mr-10" />
+        깃허브 팔로우 탐지기
+      </header>
       {children}
     </>
   );

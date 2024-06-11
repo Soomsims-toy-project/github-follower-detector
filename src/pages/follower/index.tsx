630c7d9889582bcc6cf1c618c3914bc4c316f315
suwonthugger <ddww1029wwdd@gmail.com>
2024-06-12 02:40:12 +0900

    feat: FollowerItem 컴포넌트 구현

--- src/pages/follower/index.tsx
+++ src/pages/follower/index.tsx
@@ -1,5 +1,16 @@
+import FollowerItem from '@/components/follower/FollowerItem';
+
 const FollowerPage = () => {
-  return <div>맞팔 확인 페이지</div>;
+  return (
+    <div className="flex min-h-screen flex-grow bg-grey">
+      <p>맞팔 확인 페이지</p>
+      <div className="flex flex-col">
+        <FollowerItem githubID="suwonthugger" />
+        <FollowerItem githubID="suwonthugger" />
+        <FollowerItem githubID="suwonthugger" />
+      </div>
+    </div>
+  );
 };
 
 export default FollowerPage;

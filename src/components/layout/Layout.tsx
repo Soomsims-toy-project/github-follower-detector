43485f60b3f6e40ac031a7d85fb1ebaac6f5f9ff
se0jinYoon <tjwls1999@gmail.com>
2024-06-13 00:53:04 +0900

    style: 코드 리뷰 반영

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,13 +6,13 @@ import GithubIcon from '@/assets/svgs/github.svg';
 function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
-    <>
-      <header className="font-Pretendard flex items-center bg-dark_grey p-8 text-center text-[2rem]">
+    <div className="flex min-h-screen flex-grow flex-col">
+      <header className="flex items-center bg-dark_grey p-8 text-center text-[2rem]">
         <GithubIcon onClick={() => router.push('/')} className="mr-10 h-20 w-20 cursor-pointer" />
         깃허브 팔로우 탐지기
       </header>
       {children}
-    </>
+    </div>
   );
 }
 

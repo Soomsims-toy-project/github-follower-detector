c5c5c434adda91bfbdd6f381e99e4063d82aaf45
suwonthugger <ddww1029wwdd@gmail.com>
2024-06-12 13:31:13 +0900

    feat: ssr csr 구조 분리

--- src/pages/follower/index.tsx
+++ src/pages/follower/index.tsx
@@ -1,42 +1,72 @@
 import FollowerList from '@/components/follower/FollowerList';
 import UserProfile from '@/components/follower/UserProfile';
 
-const mockData = [
-  { userName: 'suwonthugger1', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger2', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger3', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger4', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger5', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger6', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger7', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger8', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger9', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger10', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger11', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger12', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger13', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger14', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger15', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger16', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger17', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-  { userName: 'suwonthugger18', imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4' },
-];
+import { mockData } from './_data/mock';
 
-const FollowerPage = () => {
+interface Follower {
+  userName: string;
+  imageURL: string;
+}
+
+interface UserProfileData {
+  userName: string;
+  description: string;
+  imageURL: string;
+  follower: number;
+  following: number;
+}
+
+interface FollowerPageProps {
+  userProfileData: UserProfileData;
+  initialFollowerData: Follower[];
+}
+
+export async function getServerSideProps() {
+  // userProfileData: 서버 사이드에서 데이터를 미리 받아옴 userProfile은 동적으로 계속 바뀌지 않으므로
+  // Todo: api 코드 작성 (임시로 mockData를 받아오는 코드 작성)
+  const userProfileData = {
+    userName: 'suwonthugger1',
+    description: 'thug life',
+    imageURL: 'https://avatars.githubusercontent.com/u/127329855?v=4',
+    follower: 3,
+    following: 4,
+  };
+
+  return {
+    props: {
+      userProfileData,
+      initialFollowerData: mockData, // followerData" 처음에는 서버 사이드에서 FollowerData를 받아오고 이후 csr로 동작 (팔로우 언팔로우에 따라 데이터가 바뀜)
+    },
+  };
+}
+
+const FollowerPage = ({ userProfileData, initialFollowerData }: FollowerPageProps) => {
+  // Todo: followerData 팔로우 언팔로우에 따라 csr로 상태 관리 (react-query 사용)
+
   return (
     <main className="flex min-h-screen flex-grow flex-col items-center bg-grey">
       <section className="mt-8">
         <UserProfile
-          userName={mockData[0].userName}
-          description={'thug life'}
-          imageURL={mockData[0].imageURL}
-          follower={3}
-          following={4}
+          userName={userProfileData.userName}
+          description={userProfileData.description}
+          imageURL={userProfileData.imageURL}
+          follower={userProfileData.follower}
+          following={userProfileData.following}
         />
       </section>
       <section className="mt-16 flex gap-16">
-        <FollowerList className="text-mid_blue" title="맞팔중인 사용자 수" number={48} followerInfoList={mockData} />
-        <FollowerList className="text-mid_red" title="맞팔이 아닌 사용자 수" number={48} followerInfoList={mockData} />
+        <FollowerList
+          className="text-mid_blue"
+          title="맞팔중인 사용자 수"
+          number={48}
+          followerInfoList={initialFollowerData}
+        />
+        <FollowerList
+          className="text-mid_red"
+          title="맞팔이 아닌 사용자 수"
+          number={48}
+          followerInfoList={initialFollowerData}
+        />
       </section>
     </main>
   );

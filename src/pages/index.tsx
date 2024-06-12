6448f31ecb75867d07a51477f5df4179a6166d4f
se0jinYoon <tjwls1999@gmail.com>
2024-06-13 02:55:25 +0900

    style: border-radius 수정, placeholder 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,10 @@ function MainPage() {
       <section className={`flex w-[100rem] flex-col justify-start rounded-[15px] border-2 border-black p-10`}>
         <MainButton className="mb-[1rem] w-fit bg-light_grey" text="Github 토큰 만들러 가기" onClick={onClickToken} />
         <p className={`mb-[3rem] text-[1.5rem]`}>﹒ 토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!</p>
-        <input className={`black mb-[4rem] h-[4.5rem] rounded-[10px] p-5 text-[1.5rem] text-grey`} />
+        <input
+          className={`black mb-[4rem] h-[4.5rem] rounded-[10px] p-5 text-[1.5rem] text-grey`}
+          placeholder="Github Token을 입력해주세요."
+        />
         <MainButton className="bg-blue" text="나의 맞팔 확인하기" onClick={onClickFollowList} />
       </section>
     </main>

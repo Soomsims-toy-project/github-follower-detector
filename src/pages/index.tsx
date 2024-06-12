6b8c35020ad0de2eca5d31a7e73b92d3df4b02c3
se0jinYoon <tjwls1999@gmail.com>
2024-06-13 02:35:36 +0900

    feat: 메인페이지 전체 레이아웃 잡기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,13 @@ function MainPage() {
   };
 
   return (
-    <main className={`text-blue-600 flex min-h-full flex-grow flex-col justify-center bg-grey text-[3rem]`}>
-      <h2 className="text-center">메인페이지</h2>
-      <MainButton className="w-fit bg-light_grey" text="Github 토큰 만들러 가기" onClick={onClickToken} />
-      <MainButton className="bg-blue" text="나의 맞팔 확인하기" onClick={onClickFollowList} />
+    <main
+      className={`text-blue-600 flex min-h-full flex-grow flex-col items-center justify-center bg-grey text-[3rem]`}>
+      <section className={`flex w-[100rem] flex-col justify-start rounded-[15px] border-2 border-black p-8`}>
+        <MainButton className="mb-[1rem] w-fit bg-light_grey" text="Github 토큰 만들러 가기" onClick={onClickToken} />
+        <p className={`text-[1.5rem]`}>﹒ 토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!</p>
+        <MainButton className="bg-blue" text="나의 맞팔 확인하기" onClick={onClickFollowList} />
+      </section>
     </main>
   );
 }

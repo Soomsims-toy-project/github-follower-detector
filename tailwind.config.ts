e28288093ecd5eafe3458711b713abba924141b3
se0jinYoon <tjwls1999@gmail.com>
2024-06-13 00:53:04 +0900

    style: 코드 리뷰 반영

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,27 @@
-import type { Config } from 'tailwindcss';
+  import type { Config } from 'tailwindcss';
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        Pretendard: ['Pretendard-Regular'],
-      },
-      colors: {
-        dark_grey: '#252525',
-        grey: '#313131',
-        light_grey: '#999999',
-        blue: '#3285FF',
-        light_blue: '#E7FAFE',
-        red: '#FF3233',
-        light_red: '#FFE7E6',
+  const config: Config = {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          Pretendard: ['Pretendard-Regular'],
+        },
+        colors: {
+          dark_grey: '#252525',
+          grey: '#313131',
+          light_grey: '#999999',
+          blue: '#3285FF',
+          light_blue: '#E7FAFE',
+          red: '#FF3233',
+          light_red: '#FFE7E6',
+        },
       },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [],
+  };
+  export default config;

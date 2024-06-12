b6c2a005451ee1543b7897ac171113d3e90ddf54
se0jinYoon <tjwls1999@gmail.com>
2024-06-13 03:50:32 +0900

    refactor: button 태그 Link로 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,37 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { ChangeEvent, useState } from 'react';
 
-import MainButton from '@/components/main/Button';
-
 function MainPage() {
-  const router = useRouter();
   const [token, setToken] = useState('');
-  const onClickToken = () => {
-    router.push('https://github.com/settings/tokens');
-  };
-  const onClickFollowList = () => {
-    router.push('/follower');
-    sessionStorage.setItem('token', token);
-  };
+
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setToken(e.target.value);
+  };
+
+  const onClickFollowList = () => {
+    sessionStorage.setItem('token', token);
   };
 
   return (
     <main
       className={`text-blue-600 flex min-h-full flex-grow flex-col items-center justify-center bg-grey text-[3rem]`}>
       <section className={`flex w-[100rem] flex-col justify-start rounded-[15px] border-2 border-black p-10`}>
-        <MainButton className="mb-[1rem] w-fit bg-light_grey" text="Github 토큰 만들러 가기" onClick={onClickToken} />
+        <Link
+          className="mb-[1rem] w-fit rounded-[10px] bg-light_grey p-[1rem] text-center text-2xl font-bold"
+          href="https://github.com/settings/tokens">
+          Github 토큰 만들러 가기
+        </Link>
         <p className={`mb-[3rem] text-[1.5rem]`}>﹒ 토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!</p>
         <input
           onChange={onChangeInput}
           className={`black mb-[4rem] h-[4.5rem] rounded-[10px] p-5 text-[1.5rem] text-grey`}
           placeholder="Github Token을 입력해주세요."
         />
-        <MainButton className="bg-blue" text="나의 맞팔 확인하기" onClick={onClickFollowList} />
+        <Link href={'/follower'} className={`rounded-[10px] bg-blue text-center text-2xl font-bold`}>
+          <button type="button" onClick={onClickFollowList} className="size-full p-[1rem]">
+            나의 맞팔 확인하기
+          </button>
+        </Link>
       </section>
     </main>
   );

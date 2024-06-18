175d3759df2c58fbddc2def8e0324af8428ae33e
suwonthugger <ddww1029wwdd@gmail.com>
2024-06-18 16:34:05 +0900

    remove: 불필요한 react import 삭제

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="bg-dark_grey p-8 text-center text-[2rem]">헤더</header>
-      {children}
+      <div className="flex min-h-screen flex-grow flex-col">
+        <header className="bg-dark_grey p-8 text-center text-[2rem]">헤더</header>
+        {children}
+      </div>
     </>
   );
 }
